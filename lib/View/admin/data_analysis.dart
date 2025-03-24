7fc75a769ee3cdde08b4da77ee92c467d2b815fc
yunjia-123 <limyunjia5875@gmail.com>
2025-03-25 01:14:03 +0800

    Save admin changes

---
+++ lib/View/admin/data_analysis.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DataAnalysisPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Data Analysis")),
+      body: Center(child: Text("Data Analysis Page")),
+    );
+  }
+}
